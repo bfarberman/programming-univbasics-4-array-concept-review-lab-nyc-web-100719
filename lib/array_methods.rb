42287e7ce205deffe9f5fbe06b657d54ce6a0a6a
Brad Farberman <bfarberman@gmail.com>
2019-09-19 00:31:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,4 @@
 def find_element_index(array, value_to_find)
-  element_index = nil 
-  length = array.length
-  length.times do |index| 
-    brad = array[index]
-    if value_to_find == brad
-      
-  end 
 end
 
 def find_max_value(array)
